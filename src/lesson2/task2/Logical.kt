478a84093d750fc54f0e3b130a66a566593026e0
amalykh <awa149@rambler.ru>
2018-02-23 19:12:47 +0700

    Fixed lesson 2

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -29,7 +29,7 @@ fun isNumberHappy(number: Int) =
  * Считать, что ферзи не могут загораживать друг друга.
  */
 fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int) =
-        x1 == x2 || y1 == y2 || x1 + x2 == y1 + y2 || x1 - x2 == y1 - y2
+        x1 == x2 || y1 == y2 || x1 + y1 == x2 + y2 || x1 - y1 == x2 - y2
 
 /**
  * Средняя
